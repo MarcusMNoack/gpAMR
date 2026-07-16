ee6e7f9e5d76f4782a990f7ef865a5375c821966
Marcus Michael Noack <MarcusNoack@lbl.gov>
2026-07-16 11:46:22 -0700

    flow work

--- allocate_CPUsFLOW.sh
+++ allocate_CPUsFLOW.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-salloc --nodes $1 -n $2 --qos premium --time 04:00:00 --constraint cpu --account m1516
+salloc --nodes $1 -n $2 --qos interactive --time 04:00:00 --constraint cpu --account m1516
